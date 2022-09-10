a448bee0c3385c6055f91528c04da0f60e29fec3
Cesco Ferraro <francescoaferraro@gmail.com>
2022-09-10 12:10:58 -0300

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/hello-world/" : "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
