fa76cf92aa6c1a5034b902922771504e700c061d
Cesco Ferraro <francescoaferraro@gmail.com>
2022-09-10 12:25:46 -0300

    change url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: process.env.NODE_ENV === "production" ? "/learning/" : "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
